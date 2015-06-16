5a503fcfb9e120667a3cdddaf7f84b1943afc4dd
Crawl9992 <Crawl9992@hotmail.com>
2015-06-15 17:39:21 -0700

    First checkin with FizzBuzz challenge

---
+++ ChallengesConsole/ChallengesConsole.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ProgrammingChallenges.Challenges;
+
+namespace ChallengesConsole
+{
+    class ChallengesConsole
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz.RunAll();
+        }
+    }
+}
