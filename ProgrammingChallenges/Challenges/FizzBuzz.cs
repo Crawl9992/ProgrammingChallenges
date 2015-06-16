0de726fbc97fdb9be29f086310296c5960d53384
Crawl9992 <Crawl9992@hotmail.com>
2015-06-15 17:42:16 -0700

    Second checkin including ProgrammingChallenges library with FizzBuzz challenge

---
+++ ProgrammingChallenges/Challenges/FizzBuzz.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProgrammingChallenges.Challenges
+{
+    /// <summary>
+    /// Write a program that prints the numbers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
+    /// </summary>
+    public sealed class FizzBuzz
+    {
+        private string FizzBuzzString(uint Count = 100)
+        {
+            string str = String.Empty;
+            for(uint i=1; i<=Count; i++)
+            {
+                bool three = (i % 3 == 0);
+                bool five = (i % 5 == 0);
+    
+                str += ((three ? "Fizz" : "") + (five ? "Buzz" : "") + (!three && !five ? i.ToString() : "")) + "\n";
+            }
+            return str;
+        }
+
+        public static void RunAll()
+        {
+            Console.WriteLine("FizzBuzz counting to 100:");
+            Console.Write(new FizzBuzz().FizzBuzzString(100));
+        }
+    }
+}
